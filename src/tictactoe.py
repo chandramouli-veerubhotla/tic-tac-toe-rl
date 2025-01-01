45cf035ab85e8ae54067fdc28eee1ba24479d20b
Chandramouli Veerubhotla <chandramouli.veerubhotla@gmail.com>
2025-01-01 14:01:43 +0530

    Enhanced to work with nxn grid size

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -15,33 +15,40 @@ Result = namedtuple("Result", ["symbol", "is_win", "is_draw", "game_over"])
 class TicTacToe:
     """Tic Tac Toe game class."""
 
-    def __init__(self, player_1: str = 'X', player_2: str = 'O', initial_player: str = 'X'):
+    def __init__(self, player_1: str = 'X', player_2: str = 'O', initial_player: str = 'X', n: int = 3):
+        """Initialize the game."""
+        # Create constants
+        self.NUM_ROWS = self.NUM_COLS = n
+        self.GRID_SIZE = (self.NUM_ROWS, self.NUM_COLS)
+        self.TOTAL_POSSIBLE_MOVES = self.NUM_ROWS * self.NUM_COLS
+        self.TOTAL_POSSIBLE_COMBINATIONS = self.NUM_ROWS ** self.TOTAL_POSSIBLE_MOVES
+
         # Maintain state of the game
         self.players = [player_1, player_2]
         self.can_play = True
-        self.board = np.full((3, 3), '', dtype=str)
+        self.board = np.full(self.GRID_SIZE, '', dtype=str)
         self.current_player = self.players.index(initial_player)
     
     def reset(self, initial_player: str = 'X'):
         """Reset the game."""
         self.can_play = True
-        self.board = np.full((3, 3), '', dtype=str)
+        self.board = np.full(self.GRID_SIZE, '', dtype=str)
         self.current_player = self.players.index(initial_player)
         
     def __str__(self):
         """Return a string representation of the board."""
-        return '\n'.join([' '.join(row) for row in self.board])
+        return '\n'.join([' '.join(cell if cell != '' else '.' for cell in row) for row in self.board])
 
     def play(self, index: int) -> Result:
         """Play a move on the board."""
         if not self.can_play:
             raise RuntimeError(f'Game is over. Please reset the game.')
-        if not (0 <= index <= 8):
-            raise ValueError(f'Invalid move. Index should be between 0 and 8.')
+        if not (0 <= index <= self.TOTAL_POSSIBLE_MOVES - 1):
+            raise ValueError(f'Invalid move. Index should be between 0 and {self.TOTAL_POSSIBLE_MOVES - 1}.')
 
         # Convert index to row and col
         # Ensure the cell is empty
-        row, col = divmod(index, 3)
+        row, col = divmod(index, self.NUM_COLS)
         if not self.board[row, col] == '':
             raise ValueError("Invalid move. Cell already occupied.")
 
@@ -67,7 +74,7 @@ class TicTacToe:
         """Check if the game is won or drawn."""
         def is_win():
             # Check rows and columns
-            for i in range(3):
+            for i in range(self.NUM_ROWS):
                 if np.all(self.board[i, :] == player_symbol) or np.all(self.board[:, i] == player_symbol):
                     return True
             # Check diagonals
