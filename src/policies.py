423331e13aa26e7b43a2b6554ec073ad4afa448f
Chandramouli Veerubhotla <chandramouli.veerubhotla@gmail.com>
2025-01-14 16:30:31 +0530

    Converted policies to funcs
    As Q updates during learning, using class to maintain state is not good option. Hence migrating all policies to functions without maintaining the state

--- src/policies.py
+++ src/policies.py
@@ -1,78 +1,35 @@
 """Policies"""
-import abc
 import numpy as np
 from typing import Optional, List
 
 
-class Policy(abc.ABC):
-
-    def __init__(self, Q: np.ndarray):
-        if len(Q.shape) != 2:
-            raise ValueError("Q-values must be a 2D array")
-        # Maintain Q function
-        self._Q = Q
-        self._num_states = Q.shape[0]
-        self._num_actions = Q.shape[1]
-
-    @property
-    def num_states(self):
-        return self._num_states
-
-    @property
-    def num_actions(self):
-        return self._num_actions
-
-    @property
-    def Q(self):
-        return self._Q
-
-    @abc.abstractmethod
-    def __call__(self, state: int, feasible_actions: Optional[List[int]] = None) -> tuple[int, float]:
-        raise NotImplementedError()
+def random_policy(Q: np.ndarray, state: int, feasible_actions: Optional[List[int]] = None) -> tuple[int, float]:
+    if feasible_actions is None:
+        feasible_actions = list(range(Q.shape[1]))
+    action = np.random.choice(feasible_actions)
+    value = float(Q[state, action])
+    return action, value
 
 
-class RandomPolicy(Policy):
+def greedy_policy(Q: np.ndarray, state: int, feasible_actions: Optional[List[int]] = None) -> tuple[int, float]:
+    # Ensure feasible_actions contain indexes within the Q-values array
+    if feasible_actions is None:
+        feasible_actions = list(range(Q.shape[1]))
 
-    def __call__(self, state: int, feasible_actions: Optional[List[int]] = None) -> tuple[int, float]:
-        if feasible_actions is None:
-            feasible_actions = list(range(self._num_actions))
-        action = np.random.choice(feasible_actions)
-        value = float(self.Q[state, action])
-        return action, value
+    # Create mask for actions to filter out infeasible actions
+    mask = np.zeros(Q.shape[1], dtype=bool)
+    mask[feasible_actions] = True
+    masked_state_values = np.where(mask, Q[state, :], -np.inf)
 
-
-class GreedyPolicy(Policy):
-    """Greedy policy: Selects the action with the highest Q-value for a given state."""
-
-    def __call__(self, state: int, feasible_actions: Optional[List[int]] = None) -> tuple[int, float]:
-        action: int = np.argmax(self.Q[state, :]).item()
-        value: float = float(self.Q[state, action])
-        return action, value
+    # Apply greedy selection and get action, value
+    action: int = np.argmax(masked_state_values).item()
+    value: float = float(Q[state, action])
+    return action, value
 
 
-class EpsilonGreedyPolicy(RandomPolicy, GreedyPolicy):
-    """Epsilon-greedy policy: Selects the action with the highest Q-value with probability (1 - epsilon)
-    and selects a random action with probability epsilon."""
-
-    def __init__(self, Q: np.ndarray, epsilon: float = 0.1):
-        super().__init__(Q)
-        # Epsilon value
-        self._epsilon = None
-        self.epsilon = epsilon
-
-    @property
-    def epsilon(self):
-        return self._epsilon
-
-    @epsilon.setter
-    def epsilon(self, value: float):
-        if not 0 <= value <= 1:
-            raise ValueError("Epsilon must be in the range [0, 1]")
-        self._epsilon = value
-
-    def __call__(self, state: int, feasible_actions: Optional[List[int]] = None) -> tuple[int, float]:
-        if np.random.rand() < self._epsilon:
-            # Random Policy
-            return super(RandomPolicy, self).__call__(state, feasible_actions=feasible_actions)
-        # Greedy Policy
-        return super(GreedyPolicy, self).__call__(state, feasible_actions=feasible_actions)
+def epsilon_greedy_policy(Q: np.ndarray, state: int, feasible_actions: Optional[List[int]] = None, epsilon: float = 0.1) -> tuple[int, float]:
+    if np.random.rand() < epsilon:
+        # Random Policy
+        return random_policy(Q, state, feasible_actions=feasible_actions)
+    # Greedy Policy
+    return greedy_policy(Q, state, feasible_actions=feasible_actions)
